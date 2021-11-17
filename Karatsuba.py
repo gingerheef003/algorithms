447411ac6c4c73315cfb28a29e4278a3b97a4ffd
Ginger Heef <gingerheef003@gmail.com>
2021-11-17 08:32:19 +0530

    Karatsuba failed

--- Karatsuba.py
+++ Karatsuba.py
@@ -1,19 +1,16 @@
 n = int(input("Enter n: "))
 m = int(input("Enter m: "))
 
-def mul(x, y):
+def mult(x, y):
     l = len(str(x))
     k = len(str(y))
     if l==1 or k==1:
         return x*y
     else:
-        ac = mul(int(str(x)[0:l//2]), int(str(y)[0:k//2]))
-        bd = mul(int(str(x)[l//2:l]), int(str(y)[k//2:l]))
-        adbc = mul(int(str(x)[0:l//2])+int(str(x)[l//2:l]), int(str(y)[0:k//2])+int(str(y)[k//2:l])) - (ac + bd)
+        ac = mult(int(str(x)[0:l//2]), int(str(y)[0:k//2]))
+        bd = mult(int(str(x)[l//2:l]), int(str(y)[k//2:k]))
+        ad_bc = mult(int(str(x)[0:l//2])+int(str(x)[l//2:l]), int(str(y)[0:k//2])+int(str(y)[k//2:k])) - (ac + bd)
 
-        if l%2==0:
-            return ac*10**l + adbc*10**(l//2) + bd
-        else:
-            return ac*10**(l+1) + adbc*10**(l//2+1) + bd
+        return ac*10**(l+k-l//2-k//2) + bd + 
 
-print("n * m =", mul(n,m))
+print("n * m =", mult(n,m))
